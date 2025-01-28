01a0dc6953ef0e7b6c7b80e7d1752682c91c1abc
Abhijeet <aydv760@gmail.com>
2025-01-28 23:42:25 +0530

    andj

---
+++ Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public static void main(String[] args) {
+        String word1 = "abc";
+        String word2 = "pqr";
+
+        Solution solution = new Solution();
+        String mergedString = solution.mergeAlternately(word1, word2);
+
+        System.out.println("Merged String: " + mergedString); 
+    }
+
+    public String mergeAlternately(String word1, String word2) {
+        // ... (rest of the mergeAlternately method as before)
+    }
+}
---
+++ mergeStr.java
@@ -0,0 +1,13 @@
+ class mergeStr {
+    public String mergeAlternately(String word1, String word2) {
+        
+                 System.out.println();
+       
+        
+    }
+     
+    public static void main(String args[]){
+        mergeStr s = new mergeStr();
+        s.mergeAlternately("abc","pqr");
+    }
+}
